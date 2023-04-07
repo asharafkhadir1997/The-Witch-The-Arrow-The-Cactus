7bdf565343c653dc09d63a2df10d501fbd05ee10
Asharaf Khadir <asharafkhadir1997@gmail.com>
2023-04-07 18:33:44 +0530

    score function added

--- script.js
+++ script.js
@@ -6,23 +6,52 @@ let characterTop;
 let blockTop;
 let blockleft;
 let jumpInterval;
+let score = 0;
+let topScore;
+
+addEventListener("animationiteration", () => {
+  score++;
+});
+
+localStorage.setItem("Name", "Asharaf Khadir");
+localStorage.setItem("TopScore", 20);
+let getScore = localStorage.getItem("TopScore");
+
+function result() {
+  let text = `Game Over Score is ${score} Press Okay to Restrat`;
+  if (confirm(text) == true) {
+    window.location.reload();
+    score = 0;
+  } else {
+    window.location.reload();
+    score = 0;
+  }
+}
 
 setInterval(() => {
+  if (score > getScore) {
+    localStorage.setItem("TopScore", score);
+    topScore = score;
+  } else {
+    topScore = getScore;
+  }
+
   characterTop = parseInt(
     window.getComputedStyle(character).getPropertyValue("top")
   );
   blockTop = parseInt(window.getComputedStyle(block).getPropertyValue("top"));
-  blockleft = parseInt(
-    window.getComputedStyle(block).getPropertyValue("left")
-  );
-  if (characterTop > 280 && blockleft < 30 ) {
-    console.log(true);
+  blockleft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+  if (characterTop > 280 && blockleft < 30) {
+    return result();
+  }
+  if(characterTop < 20 || characterTop > 325){
+    
+    return result();
   }
   if (characterTop < 330 && jumping == 0) {
     character.style.top = `${characterTop + 3}px`;
   }
 }, 50);
-
 
 function jump() {
   jumping = 1;
